409878493ce1ef5146bd7fd390b6a904bd323654
sys106 <sys106@psu.edu>
2022-06-28 16:45:48 -0400

    vr and look controls reverted

--- scripts/virtual-tour.js
+++ scripts/virtual-tour.js
@@ -212,7 +212,8 @@ jQuery( document ).ready(function(){
     a_camera.setAttribute('cursor', { rayOrigin: 'mouse' } );
     a_camera.setAttribute('reverse-mouse-drag', true );
     a_camera.setAttribute('zoom', '1' );
-    a_camera.setAttribute('wasd-controls-enabled', false );
+    a_camera.setAttribute('wasd-controls', { acceleration: 100 } );
+    a_camera.setAttribute('look-controls', true );
 
     var a_cameraEntityCam = document.createElement('a-entity');
     a_camera.appendChild(a_cameraEntityCam);
